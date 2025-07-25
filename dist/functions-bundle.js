4a54b041c499feec6f22908eacfb9abae84c3145
BetterSolutions.com <31119149+OfficeAddins@users.noreply.github.com>
2025-07-25 21:09:23 +0100

    Add files via upload

---
+++ dist/functions-bundle.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./functions.js ***!
+  \**********************/
+CustomFunctions.associate("ID_ADDFUNCTION", ADD_FUNCTION);
+function ADD_FUNCTION(first, second) {
+  return first + second;
+}
+/******/ })()
+;
---
+++ dist/index-bundle.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+/*!******************!*\
+  !*** ./index.js ***!
+  \******************/
+window.onload = function Start() {
+  console.log('custom function');
+  var app_1 = document.getElementById("app");
+  app_1.innerHTML = '<b>custom function</b>';
+};
+Office.initialize = function (reason) {};
+/******/ })()
+;
